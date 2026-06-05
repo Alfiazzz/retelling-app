28edf17fee74b134a0044cf6848d0c9017b8951b
Альфия <5005404@mail.ru>
2026-06-05 11:38:44 +0300

    Update emailService.js

--- backend/src/services/emailService.js
+++ backend/src/services/emailService.js
@@ -4,8 +4,9 @@ import nodemailer from 'nodemailer'
 function createTransport() {
   return nodemailer.createTransport({
     host: 'mail.pereskazka-ai.ru',
-    port: 25,
+    port: 587,
     secure: false,
+    requireTLS: true,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
